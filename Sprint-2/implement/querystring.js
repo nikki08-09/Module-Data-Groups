c269504a21a771842a5dbca634a32ffab483a01f
Nikita <nikita.belgium@gmail.com>
2026-05-11 16:06:13 +0200

    fixed errors

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,23 +1,22 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
-  if (queryString[0] === "?") {
-    queryString = queryString.slice(1);
-  }
   if (!queryString) {
     return queryParams;
   }
+
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
+  }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
     if (!pair) {
       continue;
     }
-    const [rawKey, rawValue] = pair.split("=");
+    const [rawKey, ...rawValue] = pair.split("=");
     const key = decodeURIComponent(rawKey);
-    const value = decodeURIComponent(rawValue || "");
+    const value = decodeURIComponent(rawValue.join("="));
     queryParams[key] = value;
   }
 
