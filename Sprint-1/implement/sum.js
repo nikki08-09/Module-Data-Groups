778d436649ec364d694fdf3aef503b4b5bca171c
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:21:04 +0200

    Added for loop to iterate elements check if number then add to the total.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+  let total = 0;
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number") {
+      total += elements[i];
+    }
+  }
+  return total;
 }
 
 module.exports = sum;
