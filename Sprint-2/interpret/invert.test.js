3892fb2c1f8f2682ddf30e3fa529c9ec8a1b59fe
Nikita <nikita.belgium@gmail.com>
2026-05-11 16:30:26 +0200

    Added tests for empty objects.

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -5,3 +5,6 @@ test("invert should swap keys and values in the object", () => {
   expect(invert({ a: 1, b: 2 })).toEqual({ 1: "a", 2: "b" });
   expect(invert({ x: 10, y: 20 })).toEqual({ 10: "x", 20: "y" });
 });
+test("invert should handle empty objects", () => {
+  expect(invert({})).toEqual({});
+});
