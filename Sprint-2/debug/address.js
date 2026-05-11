a563f5ce007e3556854db38b8fcc035130e36274
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:49:10 +0200

    Added prediction and solution.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -13,3 +13,8 @@ const address = {
 };
 
 console.log(`My house number is ${address.houseNumber}`);
+
+// instead of logging out the house number, the code is logging out undefined. This is because the code is trying to access the houseNumber property of the address object, but it is not using the correct syntax to access it. The correct syntax to access a property of an object is to use dot notation (object.property) or bracket notation (object['property']). In this case, we should use dot notation to access the houseNumber property of the address object.
+
+// The corrected code is:
+//console.log(`My house number is ${address.houseNumber}`);
