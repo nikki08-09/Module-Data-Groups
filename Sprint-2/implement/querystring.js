cc7a6ef6b857c0c0c37b57012ba17de1250b4b18
Nikita <nikita.belgium@gmail.com>
2026-05-11 15:47:33 +0200

    Added conditions to check queryString in each case.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,10 +3,21 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+  if (queryString[0] === "?") {
+    queryString = queryString.slice(1);
+  }
+  if (!queryString) {
+    return queryParams;
+  }
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    if (!pair) {
+      continue;
+    }
+    const [rawKey, rawValue] = pair.split("=");
+    const key = decodeURIComponent(rawKey);
+    const value = decodeURIComponent(rawValue || "");
     queryParams[key] = value;
   }
 
