e6eea39895f9d6643fada36a381cbd94c2d43f6b
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:03:41 +0200

    Added test for no duplicates.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -25,7 +25,10 @@ test("given an empty array, it returns an empty array", () => {
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
-
+const arr1 = [1, 2, 3];
+test("given an array with no duplicates, it returns a copy of the original array", () => {
+  expect(dedupe(arr1)).toEqual([1, 2, 3]);
+});
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
