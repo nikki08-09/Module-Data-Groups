0726df6d340358bbfe5f929b01949a397018e565
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:11:31 +0200

    Added filter and if conditions and return max.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,9 @@
 function findMax(elements) {
+  const numbers = elements.filter((element) => typeof element === "number");
+  if (numbers.length === 0) {
+    return -Infinity;
+  }
+  return Math.max(...numbers);
 }
 
 module.exports = findMax;
