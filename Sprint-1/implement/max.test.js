d61693be03c9f892b5f78e98ddc8f40070e8e069
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:15:01 +0200

    Added tests for negative numbers.

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -45,6 +45,13 @@ test("given an array with both positive and negative numbers, returns the larges
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
+test.todo(
+  "given an array with just negative numbers, returns the closest one to zero"
+);
+const negativeNumbersArray = [-20, -5, -15, -1];
+test("given an array with just negative numbers, returns the closest one to zero", () => {
+  expect(findMax(negativeNumbersArray)).toBe(-1);
+});
 
 // Given an array with decimal numbers
 // When passed to the max function
