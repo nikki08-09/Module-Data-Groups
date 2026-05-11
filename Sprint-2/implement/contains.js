6bb5fd4fd120a5a1cd6c02d370481b4614463093
Nikita <nikita.belgium@gmail.com>
2026-05-11 15:03:35 +0200

    Added condition to check object and return property.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, prop) {
+  if (typeof obj !== "object" || obj === null) {
+    return false; // Return false for non-object inputs
+  }
+  return prop in obj;
+}
 
 module.exports = contains;
