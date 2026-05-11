3008aad0e888bb23c0937ad00321d4641c96f632
Nikita <nikita.belgium@gmail.com>
2026-05-11 16:27:07 +0200

    Answered all the questions.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,28 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
 // a) What is the current return value when invert is called with { a : 1 }
+//{a: 1}
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+//{a: 1, b: 2}
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+//{"1": "a", "2": "b"}
 
 // c) What does Object.entries return? Why is it needed in this program?
+// Object.entries returns an array of a given object's own enumerable string-keyed property [key, value] pairs. It is needed in this program to iterate over the key-value pairs of the input object so that we can swap them and create the inverted object.
 
 // d) Explain why the current return value is different from the target output
-
+// The current return value is different from the target output because in the current implementation,
+// we are assigning the value to a property named "key" in the inverted object,
+//  instead of using the actual key from the original object.
+// This means that all values will be assigned to the same property "key",
+//  resulting in an incorrect output.
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
