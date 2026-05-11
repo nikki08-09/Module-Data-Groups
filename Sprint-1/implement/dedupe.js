f84d1a1bd72c7f790e0d5d51c4ebd3ea36744db0
Nikita <nikita.belgium@gmail.com>
2026-05-11 14:00:09 +0200

    Added a empty array and iterate to each elements of the array and return results.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(list) {
+  const result = [];
+  for (let i = 0; i < list.length; i++) {
+    if (!result.includes(list[i])) {
+      result.push(list[i]);
+    }
+  }
+  return result;
+}
+
+module.exports = dedupe;
