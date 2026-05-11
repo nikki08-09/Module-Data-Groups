651ced7ebbc54f9accdf3b64af3942e7555be63b
Nikita <nikita.belgium@gmail.com>
2026-05-11 16:10:05 +0200

    Added conditions to check  frequency in a array.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(array) {
+  const counts = {};
+  if (!Array.isArray(array)) {
+    throw new TypeError("Input must be an array");
+  }
+  if (array.length === 0) {
+    return counts;
+  }
+  for (const item of array) {
+    counts[item] = (counts[item] || 0) + 1;
+  }
+  return counts;
+}
 
 module.exports = tally;
