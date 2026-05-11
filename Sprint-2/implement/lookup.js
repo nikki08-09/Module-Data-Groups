d7c8dcffb55bbf45addf7c2e06ec00743cd820f1
Nikita <nikita.belgium@gmail.com>
2026-05-11 15:31:04 +0200

    Added condition to lookup and return object.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,8 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  if (!Array.isArray(countryCurrencyPairs)) {
+    return {};
+  }
+  return Object.fromEntries(countryCurrencyPairs);
 }
 
 module.exports = createLookup;
