f1f8793c50f22692734fd4c2fa6b61daa6ee7ae8
Nikita <nikita.belgium@gmail.com>
2026-05-11 16:29:50 +0200

    Added tests for checking function s working.

---
+++ Sprint-2/interpret/invert.test.js
@@ -0,0 +1,7 @@
+const invert = require("./invert");
+
+test("invert should swap keys and values in the object", () => {
+  expect(invert({ a: 1 })).toEqual({ 1: "a" });
+  expect(invert({ a: 1, b: 2 })).toEqual({ 1: "a", 2: "b" });
+  expect(invert({ x: 10, y: 20 })).toEqual({ 10: "x", 20: "y" });
+});
