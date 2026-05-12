3dc6991c3feea42838fbfbc6c53a749d455b62ff
Nikita <nikita.belgium@gmail.com>
2026-05-12 14:20:57 +0200

    Added functions to update, shownext, showprevious, auto forward, auto backward and stop the slides.

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,43 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
+const imgElement = document.getElementById("carousel-img");
+const backwardBtn = document.getElementById("backward-btn");
+const forwardBtn = document.getElementById("forward-btn");
+const autoBackwardBtn = document.getElementById("auto-btn");
+const stopBtn = document.getElementById("stop-btn");
+const autoForwardBtn = document.getElementById("auto-forward-btn");
 
+let index = 0;
+let intervalId;
+function updateImage() {
+  imgElement.src = images[index];
+}
+function showNextImage() {
+  index = (index + 1) % images.length;
+  updateImage();
+}
+function showPreviousImage() {
+  index = (index - 1 + images.length) % images.length;
+  updateImage();
+}
+function startAutoForward() {
+  stopAuto();
+  intervalId = setInterval(showNextImage, 1000);
+}
+function startAutoBackward() {
+  stopAuto();
+  intervalId = setInterval(showPreviousImage, 1000);
+}
+function stopAuto() {
+  clearInterval(intervalId);
+}
+backwardBtn.addEventListener("click", showPreviousImage);
+forwardBtn.addEventListener("click", showNextImage);
+autoBackwardBtn.addEventListener("click", startAutoBackward);
+autoForwardBtn.addEventListener("click", startAutoForward);
+stopBtn.addEventListener("click", stopAuto);
 
 // Write your code here
